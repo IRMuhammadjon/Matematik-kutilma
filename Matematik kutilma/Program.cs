04b258a6f78ab8cabc646aea3d341e7b50fc68b7
IRMuhammadjon <102463648+IRMuhammadjon@users.noreply.github.com>
2022-06-08 12:11:31 +0500

    Add project files.

---
+++ Matematik kutilma/Program.cs
@@ -0,0 +1,12 @@
+﻿double  a, b=0, c;
+int n;
+Console.WriteLine("nechhi marta takrorlansin: ");
+n = int.Parse(Console.ReadLine());
+
+for(int i=0; i < n; i++)
+{
+    a = (i + 1) * Math.Pow((2/Math.PI),(i+1));
+    b = b + a;
+    
+}
+Console.WriteLine("Matematik kutilmamiz M(x) = " + b);
